700f4f2b1dca5ca3c4b9e4f4cf4b3bdee02d1f5d
Nguyen Ngoc Quyet <quyetnguyen@Nnqs-MacBook-Pro.local>
2026-04-30 18:21:03 +0700

    update: clean App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import Navigation from './components/navigation';
 
 export default function App() {
